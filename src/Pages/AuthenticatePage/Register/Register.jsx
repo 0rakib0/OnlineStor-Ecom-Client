c87745bd8d4b91777bd3e9fa04c7d6e562d9e54b
rakib <hassanrakibul926@gmail.com>
2023-12-23 18:54:16 +0600

    create registarion page

---
+++ src/Pages/AuthenticatePage/Register/Register.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom"
+
+const Register = () => {
+    return (
+        <div className="w-11/12 md:w-3/4 mx-auto md:mt-4 mt-6">
+            <div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <div className="bg-base-200 shadow-lg md:w-6/12 mx-auto p-4 rounded-lg">
+                    <form>
+                        <h4 className="text-center text-2xl font-semibold">Login Account</h4>
+                        <label className="form-control w-full">
+                            <div className="label">
+                                <span className="label-text">Email</span>
+                            </div>
+                            <input type="email" placeholder="Email" name="email" className="input input-bordered w-full" />
+                        </label>
+                        <label className="form-control w-full">
+                            <div className="label">
+                                <span className="label-text">Password</span>
+                            </div>
+                            <input type="password" placeholder="Password" name="password" className="input input-bordered w-full" />
+                        </label>
+                        <label className="form-control w-full mt-6 pb-6">
+                            <input type="submit" value='Login' className="input input-bordered w-full bg-gradient-to-r from-[#344e7d] via-[#53718b] to-[#41649f] text-white" />
+                        </label>
+                        <p>Do not Have Account? <Link className="text-blue-400 pb-4" to='/register'>Register</Link></p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Register
--- src/Routs/Routs.jsx
+++ src/Routs/Routs.jsx
@@ -1,27 +1,32 @@
 import {
-    createBrowserRouter,
-  } from "react-router-dom";
+  createBrowserRouter,
+} from "react-router-dom";
 import Root from "../Rooot/Root";
 import Home from "../Pages/HomePage/Home/Home";
 import SingleProduct from "../Pages/SingleProductView/SingleProduct";
+import Register from "../Pages/AuthenticatePage/Register/Register";
 
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Root></Root>,
-      children: [
-        {
-          path:'/',
-          element: <Home></Home>
-        },
-        {
-          path:'/view-product/:id',
-          element: <SingleProduct></SingleProduct>
-        }
-      ]
-    },
-  ]);
-  
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/view-product/:id',
+        element: <SingleProduct></SingleProduct>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      }
+    ]
+  },
+]);
 
-  export default router
+
+export default router
